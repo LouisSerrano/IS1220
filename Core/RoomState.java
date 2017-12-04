1cc5618566f9f5eaec9808da87c75d817f8e9215
tdebutler <33728652+tdebutler@users.noreply.github.com>
2017-12-04 21:08:26 +0100

    RoomState

---
+++ Core/RoomState.java
@@ -0,0 +1,18 @@
+package core;
+
+public enum RoomState {
+
+	AVAILABLE("AVAILABLE"),
+	USED("USED");
+	
+	private String state;
+	
+	RoomState(String state) {
+		this.state = state;
+	}
+	
+	public String getState() {
+		return state;
+	}
+	
+}
