d407444b5f7a0a4d0325b34db56ca54c225e6adb
tdebutler <33728652+tdebutler@users.noreply.github.com>
2017-12-05 16:26:06 +0100

    Updated ED

--- Core/EmergencyDepartment.java
+++ Core/EmergencyDepartment.java
@@ -1,7 +1,8 @@
-package Core;
+package core;
 import java.util.ArrayList;
 
-import Event.*;
+import event.*;
+import core.room.*;
 
 public class EmergencyDepartment {
 	private String name;
@@ -13,85 +14,7 @@ public class EmergencyDepartment {
 	private ArrayList<Transporter> TransporterList;
 	private EnabledEvent EnabledEvent;
 	private EventQueue eventqueue;
-	private Registration registration;
-	private Installation installation;
-	private Consultation consultation;
-	private Transportation transportation;
-	private MRI mri ;
-	private XRAY xray;
-	private Blood blood;
 	
-	
-	public Registration getRegistration() {
-		return registration;
-	}
-
-
-	public Installation getInstallation() {
-		return installation;
-	}
-
-
-	public Consultation getConsultation() {
-		return consultation;
-	}
-
-
-	public Transportation getTransportation() {
-		return transportation;
-	}
-
-
-	public MRI getMri() {
-		return mri;
-	}
-
-
-	public XRAY getXray() {
-		return xray;
-	}
-
-
-	public Blood getBlood() {
-		return blood;
-	}
-
-
-	public void setMri(MRI mri) {
-		this.mri = mri;
-	}
-
-
-	public void setXray(XRAY xRay) {
-		this.xray = xRay;
-	}
-
-
-	public void setBlood(Blood blood) {
-		this.blood = blood;
-	}
-
-
-	public void setRegistration(Registration registration) {
-		this.registration = registration;
-	}
-
-
-	public void setInstallation(Installation installation) {
-		this.installation = installation;
-	}
-
-
-	public void setConsultation(Consultation consultation) {
-		this.consultation = consultation;
-	}
-
-
-	public void setTransportation(Transportation transportation) {
-		this.transportation = transportation;
-	}
-
-
 	public EmergencyDepartment(){
 		this.name="ED";
 		this.simTme=0;
@@ -104,54 +27,156 @@ public class EmergencyDepartment {
 		this.eventqueue=new EventQueue();
 	}
 	
-	
-	public Nurse getFreeNurse(){
-		Nurse result = null;
-		for(Nurse nurse: this.NurseList){
-			if (nurse.getHumanResourceState().equals("IDLE")){
-				result= nurse;
-				break;
-			}
+	/**
+	 * Private constructor that creates a ED Singleton with given characteristic numbers :
+	 * @param transporterNb
+	 * @param nurseNb
+	 * @param physicianNb
+	 * @param boxRoomNb
+	 * @param shockRoomNb
+	 * @param labRoomNb
+	 * @param mriRoomNb
+	 * @param xRayRoomNb
+	 */
+	private EmergencyDepartment(int transporterNb, int nurseNb, int physicianNb, int boxRoomNb, 
+								int shockRoomNb, int labRoomNb, int mriRoomNb, int xRayRoomNb){
+		this.name="ED";
+		this.simTme=0;
+		this.PatientList=new ArrayList<Patient>();
+		this.TransporterList=new ArrayList<Transporter>();
+		this.NurseList= new ArrayList<Nurse>();
+		this.PhysicianList= new ArrayList<Physician>();
+		this.RoomList=new ArrayList<Room>();
+		this.EnabledEvent= new EnabledEvent();
+		this.eventqueue=new EventQueue();
+		
+		for (int i = 1; i < transporterNb; i++) {
+			Transporter t = new Transporter("", "");
+			TransporterList.add(t);
 		}
-		return result;
+		for (int i = 1; i < nurseNb; i++) {
+			Nurse n = new Nurse("", "");
+			NurseList.add(n);
+		}
+		for (int i = 1; i < physicianNb; i++) {
+			Physician p = new Physician("", "");
+			PhysicianList.add(p);
+		}
+		for (int i = 1; i < boxRoomNb; i++) {
+			BoxRoom r = new BoxRoom("");
+			RoomList.add(r);
+		}
+		for (int i = 1; i < shockRoomNb; i++) {
+			ShockRoom r = new ShockRoom("");
+			RoomList.add(r);
+		}
+		for (int i = 1; i < labRoomNb; i++) {
+			LaboratoryRoom r = new LaboratoryRoom("");
+			RoomList.add(r);
+		}
+		for (int i = 1; i < mriRoomNb; i++) {
+			MriRoom r = new MriRoom("");
+			RoomList.add(r);
+		}
+		for (int i = 1; i < xRayRoomNb; i++) {
+			RadiographyRoom r = new RadiographyRoom("");
+			RoomList.add(r);
+		}
+	}
+	
+	
+	//#### ED CHARACTERISTIC NUMBERS ####
+	
+	private static int transporterNb;
+	private static int nurseNb;
+	private static int physicianNb; 
+	private static int boxRoomNb; 
+	private static int shockRoomNb; 
+	private static int labRoomNb; 
+	private static int mriRoomNb; 
+	private static int xRayRoomNb;
+	
+	/**
+	 * Pre-initialised ED unique instance
+	 */
+	private static EmergencyDepartment INSTANCE = new EmergencyDepartment(transporterNb, 
+			nurseNb, physicianNb, boxRoomNb, shockRoomNb, labRoomNb, mriRoomNb, xRayRoomNb);
+
+	//#### CHARACTERITIC NUMBERS SETTERS ####
+	
+	public void setTransporterNb(int nb){
+		transporterNb = nb;
+	}
+	
+	public void setNurseNb(int nb){
+		nurseNb = nb;
+	}
+	
+	public void setPhysicianNb(int nb){
+		physicianNb = nb;
+	}
+	
+	public void setBoxRoomNb(int nb){
+		boxRoomNb = nb;
+	}
+	
+	public void setShockRoomNb(int nb){
+		shockRoomNb = nb;
+	}
+	
+	public void setLabRoomNb(int nb){
+		labRoomNb = nb;
+	}
+	
+	public void setMriRoomNb(int nb){
+		mriRoomNb = nb;
+	}
+	
+	public void setXrayRoomNb(int nb){
+		xRayRoomNb = nb;
+	}
+	
+	/**
+	 * To get the unique instance of the ED
+	 * @return
+	 */
+	public static EmergencyDepartment getEDInstance() {
+		return INSTANCE;
+	}
+	
+	/*
+	public Nurse getFreeNurse(){
 		
 	}
 	
 	public Physician getFreePhysician(){
-		Physician result = null;
-		for(Physician physician: this.PhysicianList){
-			if (physician.getHumanResourceState().equals("IDLE")){
-				result = physician;
-				break;
-				}
-		}
-		return result;
+		
 	}
 	
 	public Room getFreeRoom(){
-		Room result = null;
-
-		for( Room room : this.RoomList){
-			if (room.getRoomState().equals("ILDE")){
-				result= room;
-				break;
-			}
-		}
-		return result;
 		
 	}
 	
 	public Transporter getFreeTransporter(){
-		Transporter result = null;
-		for(Transporter transporter: this.TransporterList){
-			if (transporter.getHumanResourceState().equals("IDLE")){
-				result = transporter;
-				break;
-				}
-		}
-		return result;
 		
 	}
+	
+	public Patient getNextPatient(State state){
+		ArrayList<Patient> list= new ArrayList<Patient>();
+		Patient result = this.PatientList.get(0);
+		for(Patient patient : this.PatientList){
+			if (result.co)
+				
+			}
+			
+		}
+		
+	
+	
+	
+	
+	
+	
 	
 	
 
@@ -165,11 +190,11 @@ public class EmergencyDepartment {
 
 
 	public ArrayList<Transporter> getTranspoterList() {
-		return TransporterList;
+		return TranspoterList;
 	}
 
-	public void setTranspoterList(ArrayList<Transporter> transporterList) {
-		TransporterList = transporterList;
+	public void setTranspoterList(ArrayList<Transporter> transpoterList) {
+		TranspoterList = transpoterList;
 	}
 
 	public ArrayList<Room> getRoomList() {
@@ -240,6 +265,12 @@ public class EmergencyDepartment {
 	public void setNurseList(ArrayList<Nurse> nurseList) {
 		NurseList = nurseList;
 	}
+	*/
 	
-	
+	public String toString() {
+		return "EMERGENCY DPT : [TRANSPORTERS :" + transporterNb + ", NURSES :" + nurseNb + ", PHYSICIANS :"
+				+ physicianNb + ", BOX ROOMS :" + boxRoomNb + ", SHOCK ROOMS :" + shockRoomNb + 
+				", LABORATORY ROOMS :" + labRoomNb + ", MRI ROOMS :" + mriRoomNb + ", RADIOGRAPHY ROOMS :"
+				+ xRayRoomNb + "]";
+	}
 }
