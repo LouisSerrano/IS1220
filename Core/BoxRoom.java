db4532593ecddb3c8062f98ca204550a9ee916a7
LouisSerrano <33746360+LouisSerrano@users.noreply.github.com>
2017-12-22 17:03:01 +0100

    Add files via upload

---
+++ Core/BoxRoom.java
@@ -0,0 +1,11 @@
+package Core;
+
+public class BoxRoom extends Room {
+
+	public BoxRoom(String name) {
+		super(name, "BOX_ROOM");
+
+	}
+
+	
+}
--- Core/ShockRoom.java
+++ Core/ShockRoom.java
@@ -1,9 +1,9 @@
-package core;
+package Core;
 
 public class ShockRoom extends Room {
 
-	public ShockRoom(String name, String type) {
-		super(name, "ShockRoom");
+	public ShockRoom(String name) {
+		super(name, "SHOCK_ROOM");
 
 	}
 
